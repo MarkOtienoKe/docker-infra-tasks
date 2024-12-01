3494179df959b6e3c5acf45f89ed39859404932b
mochieng <mochieng@mfs.co.ke>
2024-12-01 14:37:29 +0300

    docker fix

--- fronend/src/App.js
+++ fronend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const BACKEND_URL = "https://backend-761958992403.africa-south1.run.app";
+const BACKEND_URL = "https://backend-829342983906.africa-south1.run.app";
 
 function App() {
   let [input, setInput] = React.useState('');
